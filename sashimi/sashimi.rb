cf4ce049bf667515f8d026371c49a28d82dadf13
Gonzalez, Evert E <evert.gonzalez@astrazeneca.com>
2020-11-17 18:31:11 -0600

    Adding unit price

--- sashimi/sashimi.rb
+++ sashimi/sashimi.rb
@@ -1,8 +1,9 @@
 class DiscountBot
-    def calcular(cantidad)
+    def calcular(cantidad, precio)
         puts "La cantidad es: #{cantidad}"
+        puts "El precio unitario es: #{precio}"
     end    
 end
 
 discount_bot = DiscountBot.new()
-puts discount_bot.calcular(ARGV[0])
+puts discount_bot.calcular(ARGV[0], ARGV[1])
