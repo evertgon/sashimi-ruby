1605c7834a78fdf6e4f308a0b558bdbbc69792f0
Gonzalez, Evert E <evert.gonzalez@astrazeneca.com>
2020-11-17 18:28:35 -0600

    Adding quantity

--- sashimi/sashimi.rb
+++ sashimi/sashimi.rb
@@ -1,5 +1,8 @@
 class DiscountBot
-    puts "Total: $100"
+    def calcular(cantidad)
+        puts "La cantidad es: #{cantidad}"
+    end    
 end
 
 discount_bot = DiscountBot.new()
+puts discount_bot.calcular(ARGV[0])
